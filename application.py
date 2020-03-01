7d7d9e7844aeb09f21db83ca132c89d368741c18
danivenk <danivenk@gmail.com>
2020-03-01 17:09:47 +0100

    Finished the regestration functionality

--- application.py
+++ application.py
@@ -22,83 +22,186 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+# create a secret_key
 app.secret_key = os.urandom(16)
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
+    """
+    renders the homepage
 
-    if "username" in session:
-        user = session.get("name")
-        return render_template("index.html", login=True, user=user)
-    else:
-        return render_template("index.html")
+    aborts if:
+        - request is anything else than a "GET" request
+
+    returns the homepage with parameters if logged on
+    """
+
+    # check if request is a "GET" request
+    if request.method == "GET":
+
+        # check if someone is logged on
+        if "username" in session:
+
+            # get username if so and render
+            user = session.get("username")
+
+            return render_template("index.html", login=True, user=user)
+        else:
+
+            return render_template("index.html")
+
+    # abort using a 405 HTTPException
+    abort(405)
 
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
+    """
+    register a new user
+
+    aborts if:
+        - no username/password/retype password is given (400)
+        - user already registered (400)
+        - request is anything else than "POST" or "GET"
+
+    returns the register form again if the retype password and password
+        weren't the same or the request was a "GET" request. If registration
+        was successfull it redirects (303) to "/".
+    """
+
+    # check if request was a "POST" or a "GET" request
     if request.method == "POST":
+
+        # get all values from the submitted form
         username = request.form.get("register_username")
         password = request.form.get("register_password")
         rpassword = request.form.get("register_rpassword")
 
+        # if the given passwords aren't the same rerender the template
         if password != rpassword:
             return render_template("register.html", message="passwords weren't"
                                    " the same...")
 
+        # if no username/password/retype password were given abort (400)
         if not username or not password or not rpassword:
+
+            # abort using a 400 HTTPException
             abort(400, "No username/password specified")
 
+        # search database for user
         user = db.execute("SELECT * FROM accounts "
                           "WHERE username=:username",
                           {"username": username}).fetchall()
 
-        if len(user) == 1:
+        # if username was found in the database abort (400)
+        if len(user) >= 1:
+
+            # abort using a 400 HTTPException
             abort(400, "User already registered")
 
+        # hash password
         password = security.hash_psswd(password)
 
+        # add username and password to database and commit
         db.execute("INSERT INTO accounts (username, password) "
                    "VALUES (:username, :password)",
                    {"username": username, "password": password})
         db.commit()
 
-        return redirect("/", 307)
+        return redirect("/", 303)
 
-    return render_template("register.html")
+    elif request.method == "GET":
+
+        return render_template("register.html")
+
+    # abort using a 405 HTTPException
+    abort(405)
 
 
 @app.route("/login", methods=["POST"])
 def login():
-    # get values from submitted form
-    username = request.form.get("username")
-    password = request.form.get("password")
-    user = db.execute("SELECT password FROM accounts "
-                      "WHERE username=:username",
-                      {"username": username}).fetchall()
+    """
+    logs on using the user posted login details
 
-    # make sure the required values are given else give 400 error
-    if not username or not password:
-        abort(400, "No username/password specified")
-    if len(user) != 1:
-        abort(401, "Login failed")
-        return redirect("/", 307)
+    aborts if:
+        - no username/password is specified (400)
+        - the database gives more or less than 1 account back (401)
+        - the credentials are invalid (401)
+        - method is anthing else than POST
 
-    login_validity = security.compare_hash(user[0].password, password)
+    returns a redirect (303) to "/"
 
-    if login_validity is True:
-        session["username"] = username
-    else:
-        abort(401, "Login failed")
+    used session for loggin in and out as in:
+        https://flask.palletsprojects.com/en/1.1.x/quickstart/#sessions
+    """
 
-    return redirect("/", code=303)
+    # check if request is a "POST" request
+    if request.method == "POST":
+
+        # get values from submitted form
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # find user in database
+        user = db.execute("SELECT password FROM accounts "
+                          "WHERE username=:username",
+                          {"username": username}).fetchall()
+
+        # make sure the required values are given else give 400 error
+        if not username or not password:
+
+            # abort using a 400 HTTPException
+            abort(400, "No username/password specified")
+
+        # check if the username returns more or less 1 user in database
+        if len(user) != 1:
+
+            # abort using a 401 HTTPException
+            abort(401, "Login failed")
+            return redirect("/", 303)
+
+        # check if the given password matches the one from the databes
+        login_validity = security.compare_hash(user[0].password, password)
+
+        # if it is the same as in the database add username to the session
+        if login_validity is True:
+            session["username"] = username
+        else:
+
+            # abort using a 401 HTTPException
+            abort(401, "Login failed")
+
+        return redirect("/", code=303)
+
+    # abort using a 405 HTTPException
+    abort(405)
 
 
-@app.route("/logout")
+@app.route("/logout", methods=["GET"])
 def logout():
-    session.pop("username", None)
+    """
+    logs out
 
-    return redirect("/", code=303)
+    aborts if:
+        - method is anything else than GET
+
+    returns a redirect (303) to "/"
+
+    used session for loggin in and out as in:
+        https://flask.palletsprojects.com/en/1.1.x/quickstart/#sessions
+    """
+
+    # check if request is a "GET" request
+    if request.method == "GET":
+
+        # remove username from session
+        session.pop("username", None)
+
+        return redirect("/", code=303)
+
+    # abort using a 405 HTTPException
+    abort(405)
 
 
 @app.errorhandler(HTTPException)
@@ -109,8 +212,14 @@ def errorhandler(error):
     used same code as in the exercise similarities
 
     return error template
+
+    reused part of the code from the Survey and Similarities web apps
+        from UVA Mprog Programming 2 Module 10 - Web
     """
+
+    # split the error into the header and message
     error_message = str(error).split(":")
+
     return render_template("error.html", header=error_message[0],
                            message=error_message[1]), error.code
 
--- functions/import.py
+++ functions/import.py
@@ -2,10 +2,14 @@
 # -*- coding: utf-8 -*-
 """
 version: python 3+
-import.py
+import.py imports all books from books.csv and creates the database
 Dani van Enk, 11823526
+
+references:
+    https://cs50.harvard.edu/web/notes/3/
 """
 
+# used imports
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 import os
@@ -13,9 +17,11 @@ import csv
 
 
 def main():
+    # set up database
     engine = create_engine(os.getenv("DATABASE_URL"))
     db = scoped_session(sessionmaker(bind=engine))
 
+    # create accounts/books/reviews tables
     db.execute("CREATE TABLE accounts (id SERIAL PRIMARY KEY, "
                "username VARCHAR NOT NULL, password VARCHAR NOT NULL);")
     db.execute("CREATE TABLE books (id SERIAL PRIMARY KEY, "
@@ -26,16 +32,24 @@ def main():
                "user_id INTEGER REFERENCES accounts, rating DECIMAL, "
                "text VARCHAR NOT NULL, book_id INTEGER REFERENCES books);")
 
+    # open books.csv and make a csv read object
     f = open("../books.csv")
     reader = csv.reader(f)
+
+    # skip the headers
     next(reader, None)
+
+    # for each row in books.csv add the data in the database
     for isbn, title, author, year in reader:
         db.execute("INSERT INTO books (title, author, year, isbn) "
                    "VALUES (:title, :author, :year, :isbn)",
                    {"title": title, "author": author, "year": year,
                     "isbn": isbn})
+
+    # commit the database
     db.commit()
 
 
+# execute the main function if the program is run
 if __name__ == "__main__":
     main()
